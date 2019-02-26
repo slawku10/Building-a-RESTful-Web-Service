c614f7fca65445ae46582fe93d1ea1de6a2b5a71
Sławomir Kuśmierczyk <slawomir.kusmierczyk@gmail.com>
2019-02-26 18:11:05 +0100

    Send generated Project

---
+++ src/main/java/com/github/slawku10/restfulwebservicespring/RestfulwebservicespringApplication.java
@@ -0,0 +1,13 @@
+package com.github.slawku10.restfulwebservicespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulwebservicespringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulwebservicespringApplication.class, args);
+    }
+
+}
